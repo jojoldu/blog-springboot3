b70cec2530e5d8f922d6a261e7704ee088e8818b
jojoldu <jojoldu@gmail.com>
2024-04-05 19:06:52 +0900

    Initial commit

---
+++ src/main/java/org/springboot/blog/BlogSpringboot3Application.java
@@ -0,0 +1,13 @@
+package org.springboot.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSpringboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSpringboot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/springboot/blog/BlogSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboot.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSpringboot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
